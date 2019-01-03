acd3c45564dbbbe0c32cddfd3f2c2a3580bee848
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-01-02 17:16:20 -0800

    Don't unmute volume when fixing balance

--- set_balance.sh
+++ set_balance.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env osascript
 
-set volume output volume (output volume of (get volume settings))
+tell application "System Events"
+  set activeApp to name of first application process whose frontmost is true
+  if not ("System Preferences" is in activeApp) then
+    tell application "System Preferences"
+      reveal anchor "output" of pane id "com.apple.preference.sound"
+    end tell
+
+    tell application "System Events"
+      tell application process "System Preferences"
+        repeat until exists tab group 1 of window "Sound"
+        end repeat
+        tell slider 1 of group 1 of tab group 1 of window 1
+          set value to 0.5
+        end tell
+      end tell
+    end tell
+
+    tell application "System Preferences"
+      quit
+    end tell
+  end if
+end tell
