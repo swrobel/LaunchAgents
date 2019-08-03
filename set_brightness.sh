3c1e805676f50b1c8acd1318c86a0a2c1914473e
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-08-03 13:34:35 -0700

    Updae brightness ratio

--- set_brightness.sh
+++ set_brightness.sh
@@ -1,11 +1,12 @@
-# Set secondary display brightness to match primary display (calibrated for iMac 27" & Dell P2415Q)
+# Set secondary display brightness to match primary display (calibrated for iMac 27" 2015 & Dell P2415Q)
 
 DISPLAY_STATE=`ioreg -n IODisplayWrangler | grep -i IOPowerManagement | perl -pe 's/^.*DevicePowerState\"=([0-9]+).*$/$1/'`
 
 # Only if display is awake
 if [[ $DISPLAY_STATE = "4" ]]; then
   IMAC_BRIGHTNESS=`/usr/local/bin/brightness -l 2>&1 | sed -nE 's/^display 0: brightness ([0-9.]+)$/\1/p'`
-  DELL_BRIGHTNESS=`echo "$IMAC_BRIGHTNESS * 100 + 40" | bc`
+  # iMac ~ 440 cd/m² brightness, Dell 300 cd/m² ~ 1.47/1 ratio
+  DELL_BRIGHTNESS=`echo "$IMAC_BRIGHTNESS * 147" | bc`
   date
   /usr/local/bin/ddcctl -d 1 -b $DELL_BRIGHTNESS
 fi
