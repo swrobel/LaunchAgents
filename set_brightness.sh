e59f0e0dfc3a4bcfbc9a9f1ed21f5f150ff75abd
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-08-04 17:47:13 -0700

    Adjust brightness ratio again

--- set_brightness.sh
+++ set_brightness.sh
@@ -5,8 +5,8 @@ DISPLAY_STATE=`ioreg -n IODisplayWrangler | grep -i IOPowerManagement | perl -pe
 # Only if display is awake
 if [[ $DISPLAY_STATE = "4" ]]; then
   IMAC_BRIGHTNESS=`/usr/local/bin/brightness -l 2>&1 | sed -nE 's/^display 0: brightness ([0-9.]+)$/\1/p'`
-  # iMac ~ 440 cd/m² brightness, Dell 300 cd/m² ~ 1.47/1 ratio
-  DELL_BRIGHTNESS=`echo "$IMAC_BRIGHTNESS * 147" | bc`
+  # iMac appears to be about 2x as bright as Dell on average
+  DELL_BRIGHTNESS=`echo "$IMAC_BRIGHTNESS * 200" | bc`
   date
   /usr/local/bin/ddcctl -d 1 -b $DELL_BRIGHTNESS
 fi
